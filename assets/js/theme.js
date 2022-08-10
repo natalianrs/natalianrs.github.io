55b0e1707f8f9e217a97f6699f0d522db6d27241
natalianrs <natalia.soaresnr@gmail.com>
2022-08-09 21:57:56 -0300

    Add dark light mode toggle

---
+++ assets/js/theme.js
@@ -0,0 +1,37 @@
+
+
+let toggleTheme = (theme) => {
+  if (theme == "dark") {
+    setTheme("light");
+  } else {
+    setTheme("dark");
+  }
+}
+
+
+let setTheme = (theme) =>  {
+  if (theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+  else {
+    document.documentElement.removeAttribute("data-theme");
+  }
+  localStorage.setItem("theme", theme);
+
+};
+
+
+let initTheme = (theme) => {
+  if (theme == null || theme == 'null') {
+    const userPref = window.matchMedia;
+    if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
+        theme = 'dark';
+    }
+  }
+  
+  setTheme(theme);
+}
+
+
+initTheme(localStorage.getItem("theme"));
+
